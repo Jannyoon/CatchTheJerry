6b99499a3609f629028d4b8fec7187a1d1378a48
Jannyoon <5124193@naver.com>
2024-01-30 00:51:09 +0900

    (ADD)일시정지, 재개 구현

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const JERRY_COUNT = 5;
 const CHEESE_COUNT = 5;
 const ITEM_SIZE = 70;
 
-
 const itemList = Object.freeze({
   jerry : "jerry",
   cheese : "cheese"
@@ -10,6 +9,7 @@ const itemList = Object.freeze({
 
 const scoreField = document.querySelector('.field__ButtonScore');
 const gameBtn = document.querySelector('.gameBtn');
+const icon = document.querySelector('.fa-solid');
 const gameTimer = document.querySelector('.gameTimer');
 const gameScore = document.querySelector('.gameScore');
 
@@ -24,18 +24,42 @@ const startBtn = document.querySelector('.startBtn');
 const refreshScene = document.querySelector('.refresh__scene');
 const refreshBtn = document.querySelector('.refreshBtn');
 
-//let start = false;
+let state = 'gaming';
 let timer = undefined;
-let score = JERRY_COUNT;
+let score = 0;
 const gameDuration = 10;
-
+let remainingSec = null;
 
 startBtn.addEventListener('click',()=>{
   startGame();
 })
 
+//일시정지 & 재개 구현
+gameBtn.addEventListener('click',()=>{
+  if (state==='gaming'){
+    state = 'pause';
+    icon.classList.add('fa-play');
+    icon.classList.remove('fa-pause');
+    stopTimer();
+    hideGame();
+  }
+  else if (state==='pause'){
+    state='gaming';
+    icon.classList.add('fa-pause');
+    icon.classList.remove('fa-play');
+    showGame();
+    startTimer();
+  }
+})
+
+gameField.addEventListener('click',(event)=>{
+  if (state!=='gaming') return;
+  onItemClick(event);
+})
 
 function startGame(){
+  score = 0;
+  timer = undefined;
   popupHide(firstScene); //가장 첫 스타트 화면을 지운다.
   showScoreTimerField(); //점수, 정지버튼, 타이머를 보여준다.
   initGame();
@@ -53,13 +77,27 @@ function showScoreTimerField(){
 }
 
 function startTimer(){
-  let remainingSec = gameDuration; //시간 초기화
+  if (remainingSec===null) remainingSec = gameDuration; //시간 초기화
   showRemainingTime(remainingSec);
   timer = setInterval(()=>{
     if (remainingSec<=0) return;
     showRemainingTime(--remainingSec);
   },1000);
 }
+
+function stopTimer(){
+  clearInterval(timer);
+}
+
+
+function showGame(){
+  gameField.classList.remove('game--hide');
+}
+
+function hideGame(){
+  gameField.classList.add('game--hide');
+}
+
 
 function showRemainingTime(time){
   const minute = Math.floor(time/60);
@@ -68,7 +106,7 @@ function showRemainingTime(time){
 }
 
 function showScore(){
-  gameScore.innerText = score;
+  gameScore.innerText = JERRY_COUNT-score;
 }
 
 //필드에 아이템 생성
@@ -76,6 +114,19 @@ function initGame(){
   addItem(itemList.jerry, JERRY_COUNT);
   addItem(itemList.cheese, CHEESE_COUNT);
 }
+
+function onItemClick(e){
+  const target = e.target;
+  if (target.matches(`.${itemList.jerry}`)){
+    score++;
+    target.remove();
+    showScore(); //점수 실시간으로 update함
+    console.log("제리");
+  }
+  else if (target.matches(`.${itemList.cheese}`)) console.log("치즈");
+
+}
+
 
 //아이템명과 아이템 횟수를 받아온다.
 function addItem(item, itemCount){
